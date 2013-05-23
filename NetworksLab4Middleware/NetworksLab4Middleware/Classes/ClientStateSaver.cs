25d150e8e806dd48aad850db9b12cccdba77722e
Oscar Wurdinger <owurdin1@gmail.com>
2013-05-22 21:57:16 -0700

    Created Shell for Middleware.
    Shell classes made with a few public variables for public state savers
    for the client and servers.

---
+++ NetworksLab4Middleware/NetworksLab4Middleware/Classes/ClientStateSaver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
+using System.Diagnostics;
+
+namespace NetworksLab4Middleware.Classes
+{
+    class ClientStateSaver
+    {
+        public Socket clientSocket = null;
+        public Thread clientThread = null;
+        public List<byte[]> clientMessages = null;
+        public Stopwatch clientStopWatch = null;
+
+        public ClientStateSaver()
+        {
+            clientStopWatch = new Stopwatch();
+        }
+    }
+
+    class ServerStateSaver
+    {
+        public Socket serverSocket = null;
+        public Thread serverThread = null;
+        public List<byte[]> serverMessages = null;
+        public Stopwatch serverStopWatch = null;
+
+        public ServerStateSaver()
+        {
+            serverStopWatch = new Stopwatch();
+        }
+    }
+}
